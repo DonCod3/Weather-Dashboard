a68d296e9485bb5550fcb047b85e3a5e38f60731
Shamar MELVIN <shamar@MacBook-Pro.local>
2023-08-09 18:33:09 -0400

    started function for cards

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -14,5 +14,36 @@ let saveSearchHist = function(city) {
         searches = []
         searches[0] = city;
     };
-    localStorage.setItem("searches,", JSON.stringify(searches))
+    localStorage.setItem("searches,", JSON.stringify(searches));
+};
+
+
+let checkSearch = function(city) {
+    if(!searches) {
+        saveSearchHist(city);
+    } else {
+        // prevent duplicates in searches array
+        for(let i = 0; i < searches.length; i++) {
+            if(searches[i] == city) {
+                return;
+            }
+        }
+        saveSearchHist(city);
+    }
+};
+
+
+let loadDailyWeather = function(data, i) {
+    // select all tempFuture  id's as span elements
+    let spanTemp = document.querySelectorAll("#tempFuture");
+    let lastTemp = spanTemp[spanTemp.length - 1];
+    lastTemp.textContent = data.daily[i].temp.day + "°F";
+
+    let spanWind = document.querySelectorAll("#windFuture");
+    let lastWind = spanWind[spanWind.length - 1];
+    lastWind.textContent = data.daily[i].wind_speed + " mph";
+
+    let spanHumid = document.querySelectorAll("#humidFuture");
+    let lastHumid = spanHumid[spanHumid.length - 1];
+    lastHumid.textContent = data.daily[i].humidity + "%";
 }
